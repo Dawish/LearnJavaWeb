8423d75638ff5a51bc7854b73a644ed17f30759e
742637254@qq.com <dxx13617237755>
2018-05-27 21:51:38 +0800

    update gitignore

--- MyTestProject/src/main/java/danxx/project/StudentController.java
+++ MyTestProject/src/main/java/danxx/project/StudentController.java
@@ -1,5 +1,6 @@
 package danxx.project;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,14 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/student")
 public class StudentController {
 	
+
+	private static final Logger LOGGER = Logger.getLogger(StudentController.class);
+	
 	@Autowired
     private StudentService userService;
     
-    @RequestMapping(value="/save/{id}",method=RequestMethod.GET)
+    @RequestMapping(value="/save/{onlyid}",method=RequestMethod.GET)
     @ResponseBody
-    public String saveUser(@PathVariable("id") Integer id){
-        System.out.println(id);
+    public String saveUser(@PathVariable("onlyid") Integer onlyid){
+        System.out.println(onlyid);
         userService.save();
+        LOGGER.info("student id : "+onlyid);
         return "student save success!";
     }
     
