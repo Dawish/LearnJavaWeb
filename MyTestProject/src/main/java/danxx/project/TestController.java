5ac24dbd60ff97a45ef50a2e01f99831d24e2adf
742637254@qq.com <dxx13617237755>
2018-05-27 10:24:24 +0800

    :sparkles: add RESTful api

--- MyTestProject/src/main/java/danxx/project/TestController.java
+++ MyTestProject/src/main/java/danxx/project/TestController.java
@@ -1,6 +1,7 @@
 package danxx.project;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,5 +17,17 @@ public class TestController {
 		return "danxx test project sayHello!";
 		
 	}
+	
+	/**
+	 * RESTful api
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="/user/{id}",method=RequestMethod.GET)
+	@ResponseBody
+    public String get(@PathVariable("id") Integer id){
+        System.out.println("get"+id);
+        return "user id : " + id;
+    }
 	
 }
